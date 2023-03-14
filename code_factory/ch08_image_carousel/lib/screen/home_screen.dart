f624c2b267a02cddfb275405c3e09c797174fba1
Ki Won Kim <xyz3710@gmail.com>
2023-03-14 15:58:20 +0900

    done: ch08 image carousel

--- code_factory/ch08_image_carousel/lib/screen/home_screen.dart
+++ code_factory/ch08_image_carousel/lib/screen/home_screen.dart
@@ -1,9 +1,40 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/src/services/system_chrome.dart';
+// import 'package:flutter/src/services/system_chrome.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late final PageController controller;
+  late final List<Image> _pages;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = PageController();
+    _pages = List.generate(5, (i) => i + 1)
+        .map(
+          (e) => Image.asset(
+            'assets/images/image_$e.jpeg',
+            fit: BoxFit.cover,
+          ),
+        )
+        .toList();
+
+    Timer.periodic(const Duration(seconds: 3), (timer) {
+      var page = getRotatedPage(controller.page, _pages.length);
+
+      controller.animateToPage(page,
+          duration: const Duration(milliseconds: 500), curve: Curves.ease);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,14 +43,14 @@ class HomeScreen extends StatelessWidget {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
     return Scaffold(
         body: PageView(
-      children: List.generate(5, (i) => i + 1)
-          .map(
-            (e) => Image.asset(
-              'assets/images/image_$e.jpeg',
-              fit: BoxFit.cover,
-            ),
-          )
-          .toList(),
+      controller: controller,
+      children: _pages,
     ));
+  }
+
+  int getRotatedPage(double? page, int length) {
+    var currentPage = page!.toInt();
+
+    return ++currentPage >= length ? 0 : currentPage;
   }
 }
