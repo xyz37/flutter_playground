49599268ce122a909fe70d8f2c3c55a0e7f98da2
xyz37 <xyz37@naver.com>
2023-03-14 23:59:28 +0900

    done: ch09 u & i but CupertinoDatePicker 한글 포멧이 안됨

--- code_factory/ch09_u_and_i/lib/main.dart
+++ code_factory/ch09_u_and_i/lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:ch09_u_and_i/screen/home_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(MaterialApp(
+    // flutter_localizations 패키지 추가 후 아래 지원 언어까지 설정
+    localizationsDelegates: const [
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+      DefaultCupertinoLocalizations.delegate,
+    ],
+    supportedLocales: const [
+      Locale('ko', 'KR'), // Korean, no Country code
+      Locale('en', 'US'), // English
+      // ... other locales the app supports
+    ],
     theme: ThemeData(
       fontFamily: 'sunflower',
       textTheme: const TextTheme(
--- code_factory/ch09_u_and_i/lib/screen/home_screen.dart
+++ code_factory/ch09_u_and_i/lib/screen/home_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +10,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late DateTime firstDay = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,19 +23,60 @@ class _HomeScreenState extends State<HomeScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            _DDay(),
+            _DDay(
+              onHeartPressed: () => onHeartPressed(firstDay),
+              firstDay: firstDay,
+            ),
             _CoupleImage(),
           ],
         ),
       ),
     );
   }
+
+  void onHeartPressed(DateTime initDay) {
+    // debugPrint('Heart pressed !!!');
+
+    showCupertinoDialog(
+      context: context,
+      builder: (context) {
+        return Align(
+          alignment: Alignment.bottomCenter, // 아래 중간으로 정렬
+          child: Container(
+            color: Colors.white,
+            height: 300,
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.date,
+              initialDateTime: initDay,
+              maximumDate: DateTime.now(), // 오늘 이후는 선택되지 않도록
+              onDateTimeChanged: (date) {
+                setState(() {
+                  firstDay = date;
+                });
+              },
+            ),
+          ),
+        );
+      },
+      barrierDismissible: true, // 외부 탭할 경우 다이얼로그 닫기
+    );
+  }
 }
 
 class _DDay extends StatelessWidget {
+  final GestureTapCallback onHeartPressed;
+  final DateTime firstDay;
+
+  const _DDay({
+    Key? key,
+    required this.onHeartPressed,
+    required this.firstDay,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context).textTheme;
+    final now = DateTime.now();
 
     return Column(
       children: [
@@ -42,12 +87,12 @@ class _DDay extends StatelessWidget {
         ),
         const SizedBox(height: 16),
         Text(
-          '두번째 만난날',
+          '우리 처음 만난날',
           style: theme.bodyLarge,
         ),
         const SizedBox(height: 16),
         Text(
-          '2002.02.22',
+          '${firstDay.year}.${firstDay.month}.${firstDay.day}',
           style: theme.bodyMedium,
         ),
         const SizedBox(height: 16),
@@ -57,11 +102,11 @@ class _DDay extends StatelessWidget {
             color: Colors.redAccent,
           ),
           iconSize: 60,
-          onPressed: () {},
+          onPressed: onHeartPressed,
         ),
         const SizedBox(height: 16),
         Text(
-          'D+365',
+          'D+${now.isAfter(firstDay) ? DateTimeRange(start: firstDay, end: now).duration.inDays : 0}',
           style: theme.displayMedium,
         ),
       ],
